32ac0f9c9660a28e052eb0b8ca0ac6a11d55fa78
kal5544 <kal5544@psu.edu>
2014-12-11 16:52:49 -0500

    Expanded POI edit mode menu
    Added a menu to enter POI info and also a tracker for whether the menu
    is in edit mode.

---
+++ Assets/POIMenuStateManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class POIMenuStateManager : MonoBehaviour {
+
+	public bool editModeState = false;
+
+	public void ToggleEditMode()
+	{
+		editModeState = !editModeState;
+	}
+}
