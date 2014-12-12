6549106144d79531754a220145a9a9158337b1be
IConLab <yxl5209@psu.edu>
2014-12-11 20:34:13 -0500

    Added ability to use current transform as poi data

---
+++ Assets/PullCurrentTransform.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PullCurrentTransform : MonoBehaviour {
+
+    public Transform characterTransform = null;
+    public List<InputField> poiTransformFields = new List<InputField>();
+
+    public void GatherCurrentTransform()
+    {
+        poiTransformFields[0].value = characterTransform.position.x.ToString();
+        poiTransformFields[1].value = characterTransform.position.y.ToString();
+        poiTransformFields[2].value = characterTransform.position.z.ToString();
+        poiTransformFields[3].value = characterTransform.rotation.x.ToString();
+        poiTransformFields[4].value = characterTransform.rotation.y.ToString();
+        poiTransformFields[5].value = characterTransform.rotation.z.ToString();
+        
+    }
+}
