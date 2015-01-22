bc3f74d228531b8a5f3e33244c6a4bd6b00b2bd1
IConLab <yxl5209@psu.edu>
2015-01-21 20:58:44 -0500

    Fixed POIButtonManager without file
    POIButtonManager script can now properly handle filling the
    originalHandler if a POIHandler.xml file is not found.

--- Assets/POI Scripts/POIButtonManager.cs
+++ Assets/POI Scripts/POIButtonManager.cs
@@ -8,11 +8,11 @@ using UnityEngine.EventSystems;
 
 public class POIButtonManager : MonoBehaviour {
 	
-    public static POIHandler handler = new POIHandler();
+    public static POIHandler editHandler = new POIHandler();
 
     // variable to hold the initial button and poi data when the application starts
     // will have an option in the poi menu to restore the original values
-    public POIHandler restoreHandler = new POIHandler();
+    public static POIHandler originalHandler = new POIHandler();
     public RectTransform POIList;
     public Object buttonPrefab;
     public int NumOfButtons = 0;
@@ -20,33 +20,32 @@ public class POIButtonManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
-        if (Application.isEditor)
-        {
-            if (File.Exists(POI_GlobalVariables.XMLpath))
-            {
-                foreach (Transform child in POIList.transform)
-                {
-                    Destroy(child.gameObject);
-                }
-                handler = XmlIO.Load(POI_GlobalVariables.XMLpath, typeof(POIHandler)) as POIHandler;
-                
-                handler.projectPOIs.Where(scenePOILists => scenePOILists.Count > 0)
-                       .ToList()
-                       .ForEach(sceneList => {
-                           if(sceneList[0].sceneFlag == Application.loadedLevelName)
-                           {
-                               foreach (POI point in sceneList)
-                               {
-                                   GameObject newButton = Instantiate(buttonPrefab) as GameObject;
-                                   RectTransform buttonRectTransform = newButton.transform as RectTransform;
-                                   buttonRectTransform.parent = POIList;
-                                   buttonRectTransform.localPosition = new Vector3(7.0f, -7.0f + NumOfButtons * (-buttonRectTransform.rect.height - 7.0f), 0.0f);
-                                   
-                                   newButton.GetComponent<POIInfo>().Point = point;
-                                   newButton.transform.GetComponentInChildren<Text>().text = point.buttonName;
-                                   NumOfButtons++;
-                                   POIlistHeight += buttonRectTransform.rect.height + 7.0f;
+	
+		Debug.Log(POI_GlobalVariables.XMLpath);
+	    if (File.Exists(POI_GlobalVariables.XMLpath))
+	    {
+	        foreach (Transform child in POIList.transform)
+	        {
+	            Destroy(child.gameObject);
+	        }
+	        originalHandler = XmlIO.Load(POI_GlobalVariables.XMLpath, typeof(POIHandler)) as POIHandler;
+	        
+				originalHandler.projectPOIs.Where(scenePOILists => scenePOILists.Count > 0)
+	               .ToList()
+	               .ForEach(sceneList => {
+	                   if(sceneList[0].sceneFlag == Application.loadedLevelName)
+	                   {
+	                       foreach (POI point in sceneList)
+	                       {
+	                           GameObject newButton = Instantiate(buttonPrefab) as GameObject;
+	                           RectTransform buttonRectTransform = newButton.transform as RectTransform;
+	                           buttonRectTransform.parent = POIList;
+	                           buttonRectTransform.localPosition = new Vector3(7.0f, -7.0f + NumOfButtons * (-buttonRectTransform.rect.height - 7.0f), 0.0f);
+	                           
+	                           newButton.GetComponent<POIInfo>().Point = point;
+	                           newButton.transform.GetComponentInChildren<Text>().text = point.buttonName;
+	                           NumOfButtons++;
+	                           POIlistHeight += buttonRectTransform.rect.height + 7.0f;
 
 									// code to add a listener to the button OnClicked() event
 									EventTrigger eTrigger = newButton.GetComponent<EventTrigger>();
@@ -61,27 +60,25 @@ public class POIButtonManager : MonoBehaviour {
 									// We use PointerClick so we know when the used has clicked on a button.
 									EventTrigger.Entry entry = new EventTrigger.Entry(){callback = trigger, eventID = EventTriggerType.PointerClick}; 
 									eTrigger.delegates.Add(entry);
-                               }
-                               POIList.sizeDelta = new Vector2(POIList.sizeDelta.x , POIlistHeight);
-                               POIList.localPosition = Vector3.zero;
-                           }
-                       });
-                // make a copy of the original poi information
-                // all of the modifications will be done with handler, leaving restoreHandler unchanged
-                restoreHandler = handler;
-            }
-            else
-            {
-                foreach (Transform child in POIList.transform)
-                {
-                    handler.AddPoint(child.GetComponent<POIInfo>().Point);
-                }
+	                       }
+	                       POIList.sizeDelta = new Vector2(POIList.sizeDelta.x , POIlistHeight);
+	                       POIList.localPosition = Vector3.zero;
+	                   }
+	               });
+	    }
+	    else
+	    {
+	        foreach (Transform child in POIList.transform)
+	        {
+				POI pointToAdd = child.GetComponent<POIInfo>().Point;
+	            originalHandler.AddPoint(pointToAdd);
+	        }
 
-                XmlIO.Save(handler, POI_GlobalVariables.XMLpath);
-            }
-        }
+	        XmlIO.Save(originalHandler, POI_GlobalVariables.XMLpath);
+	    }
+
+		// make a copy of the original poi information
+		// all of the modifications will be done with handler, leaving originalHandler unchanged
+		editHandler = originalHandler;
 	}// start
-
-
-
 }
