69825652ce2d7e99a2bde1e5040bb8e4d93da3be
kal5544 <kal5544@psu.edu>
2015-03-04 22:24:37 -0500

    MiniMap Upgrades
    MiniMapManager script now holds the majority of the code for the
    minimap.  There is a compassmanager script, but that will be
    incorporated into MiniMapManager soon so that it is all one script.
    
    Currently zoom and size buttons work, and the compass is positioned and
    scaled to stay in the upper right corner of the minimap and stay the
    same relative size.

---
+++ Assets/CompassManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CompassManager : MonoBehaviour {
+
+	public GameObject avatar;
+
+	private bool rotateCompass = true;
+	// Update is called once per frame
+	void Update () {
+
+		// manages rotation of the compass plane
+		if(Input.GetKeyDown("r"))
+		{
+			rotateCompass = !rotateCompass;
+		}
+		
+		if(rotateCompass)
+		{
+			transform.localEulerAngles = new Vector3(0,180-avatar.transform.eulerAngles.y,0);
+		}
+		else
+		{
+			transform.localEulerAngles = new Vector3(0,180,0);
+		}
+	}// update
+}
